0bf179c86293e1dd67172116a487975b8cca54aa
Dima Gonikman <dima.gonikman@cyberark.com>
2019-11-20 14:33:27 +0200

    Bump to v0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='AwsRegionsDictionary',
-    version='0.0.1',
+    version='0.0.2',
     description='AWS regions dictionary',
     author='go-dima',
     #url='https://github.com/go-dima',
