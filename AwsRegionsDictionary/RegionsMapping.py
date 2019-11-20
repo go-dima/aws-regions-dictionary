61759670f668eea5da9ec811fdee2fc92ee09d71
Dima Gonikman <dima.gonikman@cyberark.com>
2019-11-20 14:32:00 +0200

    Support case agnostic name/key

--- AwsRegionsDictionary/RegionsMapping.py
+++ AwsRegionsDictionary/RegionsMapping.py
@@ -1,27 +1,28 @@
 regions = {
-    "N. Virginia" : "us-east-1",
-    "Ohio" : "us-east-2",
-    "N. California" : "us-west-1",
-    "Oregon" : "us-west-2",
-    "Hong Kong" : "ap-east-1",
-    "Mumbai": "ap-south-1",
-    "Seoul" : "ap-northeast-2",
-    "Singapore" : "ap-southeast-1",
-    "Sydney" : "ap-southeast-2",
-    "Tokyo" : "ap-northeast-1",
-    "Central" : "ca-central-1",
-    "Frankfurt" : "eu-central-1",
-    "Ireland" : "eu-west-1",
-    "London" : "eu-west-2",
-    "Paris" : "eu-west-3",
-    "Stockholm" : "eu-north-1",
-    "Bahrain" : "me-south-1",
-    "Sao Paulo" : "sa-east-1"
+    "n. virginia" : "us-east-1",
+    "ohio" : "us-east-2",
+    "n. california" : "us-west-1",
+    "oregon" : "us-west-2",
+    "hong kong" : "ap-east-1",
+    "mumbai": "ap-south-1",
+    "seoul" : "ap-northeast-2",
+    "singapore" : "ap-southeast-1",
+    "sydney" : "ap-southeast-2",
+    "tokyo" : "ap-northeast-1",
+    "central" : "ca-central-1",
+    "frankfurt" : "eu-central-1",
+    "ireland" : "eu-west-1",
+    "london" : "eu-west-2",
+    "paris" : "eu-west-3",
+    "stockholm" : "eu-north-1",
+    "bahrain" : "me-south-1",
+    "sao paulo" : "sa-east-1"
 }
 
 def findRegion(regionStr):
-    if regionStr in regions.values():
-        return regionStr
-    if regionStr in regions.keys():
-        return regions[regionStr]
+    regionStr_lower = regionStr.lower()
+    if regionStr_lower in regions.values():
+        return regionStr_lower
+    if regionStr_lower in regions.keys():
+        return regions[regionStr_lower]
     raise KeyError(f"{regionStr} is not recognized.")
--- AwsRegionsDictionary/RegionsMapping_tests.py
+++ AwsRegionsDictionary/RegionsMapping_tests.py
@@ -1,16 +1,24 @@
 import unittest
-from RegionsMapping import findRegion
+from AwsRegionsDictionary.RegionsMapping import findRegion
 
 class AwsRegionDictTests(unittest.TestCase):
-    def test_findregion_by_key(self):
+    def test_findRegion_regionKeyLower_findKey(self):
         foundRegion = findRegion('eu-central-1')
         self.assertEqual(foundRegion, 'eu-central-1')
 
-    def test_findRegion_by_name(self):
+    def test_findRegion_regionKeyUpper_findKey(self):
+        foundRegion = findRegion('EU-central-1')
+        self.assertEqual(foundRegion, 'eu-central-1')
+
+    def test_findRegion_regionNameUpper_findKey(self):
         foundRegion = findRegion('Frankfurt')
         self.assertEqual(foundRegion, 'eu-central-1')
 
-    def test_findRegion_unknown_region(self):
+    def test_findRegion_regionNameLower_findKey(self):
+        foundRegion = findRegion('frankfurt')
+        self.assertEqual(foundRegion, 'eu-central-1')
+
+    def test_findRegion_unknownRegion_excention(self):
         self.assertRaises(KeyError, findRegion, 'Mars')
 
 # Run: python -m unittest discover -s AwsRegionsDictionary -p "*_tests.py"
