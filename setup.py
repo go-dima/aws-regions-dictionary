cc03b63699365250d4039515b58db62ff6428494
Dima <6072406+go-dima@users.noreply.github.com>
2020-05-25 09:46:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(name='AwsRegionsDictionary',
     version='0.0.3',
     description='AWS regions dictionary',
     author='go-dima',
-    #url='https://github.com/go-dima',
-    url='https://github.cyberng.com/dgonikman/aws-regions',
+    url='https://github.com/go-dima',
     packages=['AwsRegionsDictionary'],
     install_requires=[
   ]
